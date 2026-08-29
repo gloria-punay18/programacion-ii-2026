645b92ae02dc649d396bbc4f09d41060704dfae6
Gloria Punay <gloriapunay184@gmail.com>
2026-08-29 15:56:40 -0600

    Clase Triangulo y Clase Main
    Agrega la clase Triangulo y la clase principal Main

---
+++ Semana-07-Calculo-de-Areas-con-POO/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Figura[] figuras = new Figura[3];
+
+        figuras[0] = new Circulo("Círculo", 5.0);
+        figuras[1] = new Rectangulo("Rectángulo", 4.0, 6.0);
+        figuras[2] = new Triangulo("Triángulo", 3.0, 8.0);
+
+
+        for (Figura figura : figuras) {
+            figura.mostrarInformacion();
+            System.out.printf("Área: %.2f\n", figura.calcularArea());
+            System.out.println("-------------------------");
+        }
+    }
+}
